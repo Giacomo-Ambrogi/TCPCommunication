2a0e830de3503f03cfd0265d026b8a537aaf5492
Giacomo-Ambrogi <giacky597@gmail.com>
2026-02-09 13:21:34 +0100

    Commit Generale

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -1,7 +1,16 @@
 package Client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class MainClient {
     public static void main(String[] args) {
+        System.out.println("----- CLIENT: Avvio del client!!! -----");
 
+        try {
+            Socket socket = new Socket("localhost", 3000);
+        } catch (IOException e) {
+            System.err.println("Errore nella connessione con il server!!!");
+        }
     }
 }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,7 +1,20 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainServer {
     public static void main(String[] args) {
+        System.out.println("----- SERVER: Inizio esecuzione!!! -----");
 
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            System.out.println("SERVER: In attesa della richiesta del cliente!");
+            Socket clientSocket = server.accept();
+            System.out.println("SERVER: Il client si è connesso!");
+        } catch (IOException e) {
+            System.err.println("Errore nella connessione con il client!!!");
+        }
     }
 }
