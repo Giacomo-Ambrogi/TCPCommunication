d2217724a18b856f7c600084be5a13e5a05c86a6
aabuble <giacky597@gmail.com>
2026-03-02 13:19:42 +0100

    Commit

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -20,48 +20,6 @@ public class MainServer {
 
         server.chiudi();
 
-        /*try {
-            //comunicazione
-            ServerSocket serverSocket = new ServerSocket(3000);
-            System.out.println("SERVER: In attesa di richieste dei CLIENT!");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("SERVER: Il CLIENT si è connesso!");
-
-            //lettura
-            InputStream inputStream = clientSocket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String messaggio = br.readLine();
-            System.out.println("SERVER: Il CLIENT " + clientSocket + "ha scritto il messaggio --> " + messaggio);
-
-            BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            while(true) {
-                messaggio = br.readLine();
-
-                if (messaggio == null || messaggio.equalsIgnoreCase("esci")) {
-                    break;
-                }
-
-                System.out.println("Messaggio CLIENT: " + messaggio);
-                System.out.println("Risposta SERVER: " );
-                String risposta = tastiera.readLine();
-
-                if (risposta.equalsIgnoreCase("esci")) {
-                    break;
-                }
-
-                out.println(risposta);
-            }
-
-            inputStream.close();
-            clientSocket.close(); //chiusura data socket
-            br.close();
-            out.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }*/
-
         System.out.println("----- SERVER: Fine esecuzione!!! -----");
     }
 }
